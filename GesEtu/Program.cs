6c97ec1c7dae5ca43da782b7fa5e85a9d22f46a3
dieydiey <152610220+dieydiey@users.noreply.github.com>
2025-11-12 18:00:42 +0000

    Refactor Program.cs by removing comments
    Removed comments and cleaned up the code.

--- GesEtu/Program.cs
+++ GesEtu/Program.cs
@@ -9,17 +9,9 @@ using GesEtu.Views;
 using Microsoft.Extensions.DependencyInjection;
 
 var ioc = new ServiceCollection();
-
-// Déclaration des dépendances
 ioc.AddSingleton<IEtudiantRepository, EtudiantRepositoryImpl>();
 ioc.AddSingleton<IEtudiantService, EtudiantServiceImpl>();
-
-// Vue Console
 ioc.AddTransient<ConsoleView>();
-
-// Construction du conteneur de services
 var services = ioc.BuildServiceProvider();
-
-// Exécution du programme
 var view = services.GetRequiredService<ConsoleView>();
 view.Run();
